7789c776f926c3c8298056ef0b11ed354e337ca9
Thuy Dieu Linh Nguyen <thuy_dieu_linh.nguyen@smail.th-koeln.de>
2023-03-18 13:26:07 +0100

    add templates

--- scripts/app.py
+++ scripts/app.py
@@ -1,9 +1,9 @@
 import os
 
 from flask import Flask, render_template
-from neo4j import Graphdatabase
-from flask import Blueprint, jsonify, redirect, render_template, request
-from basicauth import basic_auth
+# from neo4j import Graphdatabase
+from flask import Flask, flash, redirect, render_template, request, url_for
+# from basicauth import basic_auth
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,9 +12,11 @@ password = os.environ.get("DATABASE_PASSWORD")
 username = os.environ.get("DATABASE_USERNAME")
 url = os.getenv("DATABASE_URL")
 
-app = Flask(__name__, static_folder="static/")
-driver = Graphdatabase.driver(url, auth=basic_auth(username, password))
+app = Flask(__name__)
+# driver = Graphdatabase.driver(url, auth=basic_auth(username, password))
 @app.route("/")
 def index():
     return render_template("index.html")
 
+if __name__ == "__main__":
+    app.run(debug=True)
