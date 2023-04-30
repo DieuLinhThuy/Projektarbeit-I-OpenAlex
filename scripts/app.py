913170547cb146f517b71c25f31c2cb1a73a82d0
Thuy Dieu Linh Nguyen <thuy_dieu_linh.nguyen@smail.th-koeln.de>
2023-04-30 20:57:35 +0200

    visualize graph with bokeh

--- scripts/app.py
+++ scripts/app.py
@@ -1,8 +1,14 @@
 import os
+import networkx as nx
+from bokeh.embed import components
+from bokeh.models.graphs import from_networkx, NodesAndLinkedEdges, EdgesAndLinkedNodes
+from bokeh.models import Plot, Range1d, MultiLine, Circle, HoverTool
+from bokeh.models.graphs import from_networkx
+from bokeh.palettes import Spectral4
+from bokeh.plotting import figure, output_file, show
 from neo4j import GraphDatabase
 from flask import Flask, render_template, request, redirect
 from dotenv import load_dotenv
-
 load_dotenv()
 
 password = os.environ.get("DATABASE_PASSWORD")
@@ -24,7 +30,6 @@ def get_data(work_title):
 
         return [dict(row) for row in result]
 
-
 # Flask-Route definieren
 @app.route('/')
 def index():
@@ -41,24 +46,36 @@ def search_work():
 @app.route('/works/<string:work_title>')
 def show_related_works(work_title):
     data = get_data(work_title)
-    graph = get_knowledge_graph(data)
-    plot = Plot(plot_width=800, plot_height=800, x_range=Range1d(-2, 2), y_range=Range1d(-2, 2))
 
-    node_hover_tool = HoverTool(tooltips=[("Name", "@name")])
-    plot.add_tools(node_hover_tool, TapTool(), BoxSelectTool(), BoxZoomTool(), ResetTool(), WheelZoomTool())
+    # Erstellen eines Graphen mit Hilfe von NetworkX
+    G = nx.Graph()
+    for row in data:
+        G.add_node(row['author_name'])
+        G.add_node(row['related_work_title'])
+        G.add_edge(row['author_name'], row['related_work_title'])
 
-    graph_renderer = from_networkx(graph, nx.spring_layout, scale=1, center=(0, 0))
-    graph_renderer.node_renderer.glyph = Circle(size=15, fill_color="skyblue")
-    graph_renderer.node_renderer.hover_glyph = Circle(size=15, fill_color="red")
-    graph_renderer.edge_renderer.glyph = MultiLine(line_color="gray", line_alpha=0.8, line_width=1)
-    graph_renderer.edge_renderer.hover_glyph = MultiLine(line_color="red", line_alpha=1, line_width=2)
+    # Erstellen des Bokeh-Plots
+    plot = Plot(plot_width=800, plot_height=600, title='Knowledge Graph',
+            x_range=Range1d(-1.1, 1.1), y_range=Range1d(-1.1, 1.1))
+
+    graph_renderer = from_networkx(G, nx.spring_layout, scale=1, center=(0, 0))
+
+    graph_renderer.node_renderer.glyph = Circle(size=15, fill_color=Spectral4[0])
+    graph_renderer.node_renderer.selection_glyph = Circle(size=15, fill_color=Spectral4[2])
+    graph_renderer.node_renderer.hover_glyph = Circle(size=15, fill_color=Spectral4[1])
+
+    graph_renderer.edge_renderer.glyph = MultiLine(line_color="#CCCCCC", line_alpha=0.8, line_width=5)
+    graph_renderer.edge_renderer.selection_glyph = MultiLine(line_color=Spectral4[2], line_width=5)
+    graph_renderer.edge_renderer.hover_glyph = MultiLine(line_color=Spectral4[1], line_width=5)
+
+    graph_renderer.selection_policy = NodesAndLinkedEdges()
+    graph_renderer.inspection_policy = NodesAndLinkedEdges()
 
     plot.renderers.append(graph_renderer)
 
-    output_file("knowledge_graph.html")
-    show(plot)
+    script, div = components(plot)
 
-    return render_template('table.html', data=data)
+    return render_template('table.html', data=data, script=script, div=div, target_id='knowledge-graph')
 
 
 if __name__ == "__main__":
